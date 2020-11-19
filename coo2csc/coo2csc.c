2cf7adaba0b12fd6ba6b8975b104035f43343b4c
Dimitris Floros <flojim4@gmail.com>
2020-11-19 13:08:18 +0200

    fixed coo2csc

--- coo2csc/coo2csc.c
+++ coo2csc/coo2csc.c
@@ -1,166 +1,115 @@
-#include <stdbool.h>
+/**
+ *   \file coo2csc.c
+ *   \brief An example of COO to CSC matrix conversion
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-void coo2csc(const uint *row_coo, const uint *col_coo, uint *row, uint *col,
-             const uint nnz, const uint rows, const uint cols, bool isOneBase) {
-  uint n = cols;
+/*****************************************************************************/
+/*                             routine definition                            */
+/*****************************************************************************/
+
+/**
+ *  \brief COO to CSC conversion
+ *
+ *  Converts a square matrix from COO to CSC format.
+ *
+ *  Note: The routine assumes the input COO and the output CSC matrix
+ *  to be square.
+ *
+ */
+void coo2csc(
+  uint32_t       * const row,       /*!< CSC row start indices */
+  uint32_t       * const col,       /*!< CSC column indices */
+  uint32_t const * const row_coo,   /*!< COO row indices */
+  uint32_t const * const col_coo,   /*!< COO column indices */
+  uint32_t const         nnz,       /*!< Number of nonzero elements */
+  uint32_t const         n,         /*!< Number of rows/columns */
+  uint32_t const         isOneBased /*!< Whether COO is 0- or 1-based */
+) {
+
+  // ----- cannot assume that input is already 0!
+  for (uint32_t l = 0; l < n+1; l++) col[l] = 0;
+
 
   // ----- find the correct column sizes
-  for (uint l = 0; l < nnz; l++) {
-    if (isOneBase) {
-      col[col_coo[l] - 1]++;
-    } else {
-      col[col_coo[l]]++;
-    }
-  }
+  for (uint32_t l = 0; l < nnz; l++)
+    col[col_coo[l] - isOneBased]++;
+
   // ----- cumulative sum
-  for (uint i = 0, cumsum = 0; i < n; i++) {
-    uint temp = col[i];
+  for (uint32_t i = 0, cumsum = 0; i < n; i++) {
+    uint32_t temp = col[i];
     col[i] = cumsum;
     cumsum += temp;
   }
   col[n] = nnz;
   // ----- copy the row indices to the correct place
-  for (uint l = 0; l < nnz; l++) {
-    uint col_l;
-    if (isOneBase) {
-      col_l = col_coo[l] - 1;
-    } else {
-      col_l = col_coo[l];
-    }
+  for (uint32_t l = 0; l < nnz; l++) {
+    uint32_t col_l;
+    col_l = col_coo[l] - isOneBased;
 
-    uint dst = col[col_l];
-    if (isOneBase) {
-      row[dst] = row_coo[l] - 1;
-    } else {
-      row[dst] = row_coo[l];
-    }
+    uint32_t dst = col[col_l];
+    row[dst] = row_coo[l] - isOneBased;
 
     col[col_l]++;
   }
   // ----- revert the column pointers
-  for (uint i = 0, last = 0; i < n; i++) {
-    uint temp = col[i];
+  for (uint32_t i = 0, last = 0; i < n; i++) {
+    uint32_t temp = col[i];
     col[i] = last;
     last = temp;
   }
-  // Debugging
-  printf("Columns\n");
-  for (int i = 0; i < cols + 1; i++) {
-    printf("%i\n", col[i]);
-  }
-  printf("Rows\n");
-  for (uint i = 0; i < nnz; i++) {
-    printf("%i\n", row[i]);
-  }
+
 }
+
+/*****************************************************************************/
+/*                 setup example and assert correct behavior                 */
+/*****************************************************************************/
 
 int main() {
 
-  // const uint nnz = 8;  // number of non-zero elements
-  const uint nnz = 7;
-  // const uint rows = 4; // number of matrix rows
-  const uint rows = 5; // number of matrix rows
+  const uint32_t nnz = 7;
+  const uint32_t n   = 5;
 
-  // const uint cols = 6; // number of matrix columns
-
-  const uint cols = 5; // number of matrix columns
-
-  uint *coo_row, *coo_col, *csr_row, *csr_col, *csc_col, *csc_row;
-  double *coo_val;
-
-  coo_row = (uint *)malloc(nnz * sizeof(uint));
-  coo_col = (uint *)malloc(nnz * sizeof(uint));
-  coo_val = (double *)malloc(nnz * sizeof(double));
-
-  csr_row = (uint *)malloc((rows + 1) * sizeof(uint));
-  csr_col = (uint *)malloc(nnz * sizeof(uint));
-
-  csc_row = (uint *)malloc(nnz * sizeof(uint));
-  csc_col = (uint *)malloc(cols + 1 * sizeof(uint));
-
-  // Example from Wikipedia (https://en.wikipedia.org/wiki/Sparse_matrix)
-
-  // Matrix:
-  // 10 20  0  0  0  0
-  //  0 30  0 40  0  0
-  //  0  0 50 60 70  0
-  //  0  0  0  0  0 80
-
-  // Expected output:
-  // csr_val: 10 20 30 40 50 60 70 80 (nnz)
-  // csr_col:  0  1  1  3  2  3  4  5 (nnz)
-  // csr_row:  0  2  4  7  8          (rows+1)
-
-  // create the example simple Matrix in 1-index
+  uint32_t * csc_row = (uint32_t *)malloc(nnz     * sizeof(uint32_t));
+  uint32_t * csc_col = (uint32_t *)malloc((n + 1) * sizeof(uint32_t));
 
   // Example from
   // https://medium.com/swlh/an-in-depth-introduction-to-sparse-matrix-a5972d7e8c86
 
-  // 0 0 0 2 0
-  //  1 0 0 0 0
-  //  0 0 1 0 0
-  //  0 0 2 1 2
+  uint32_t coo_row[7] = {1,2,3,4,5,5,5};
+  uint32_t coo_col[7] = {4,1,3,2,3,4,5};
+  uint32_t isOneBased = 1;
 
-  // Expected output:
-  // csc_val:  1 2 1 2 2 1 2 (nnz)
-  // csc_col:  0 1 2 4 6 7 (cols+1)
-  // csc_row:  1 3 2 4 0 4 4 (nnz)
-
-  bool isOneBase = false;
-
-  /*
-    coo_val[1] = 10.0;
-    coo_val[2] = 20.0;
-    coo_val[3] = 30.0;
-    coo_val[4] = 40.0;
-    coo_val[5] = 50.0;
-    coo_val[6] = 60.0;
-    coo_val[7] = 70.0;
-    coo_val[8] = 80.0;
-    coo_row[1] = 0;
-    coo_row[2] = 0;
-    coo_row[3] = 1;
-    coo_row[4] = 1;
-    coo_row[5] = 2;
-    coo_row[6] = 2;
-    coo_row[7] = 2;
-    coo_row[8] = 3;
-
-    coo_col[1] = 0;
-    coo_col[2] = 1;
-    coo_col[3] = 1;
-    coo_col[4] = 3;
-    coo_col[5] = 2;
-    coo_col[6] = 3;
-    coo_col[7] = 4;
-    coo_col[8] = 5;
-  */
-
-  coo_val[0] = 2;
-  coo_val[1] = 1;
-  coo_val[2] = 1;
-  coo_val[3] = 2;
-  coo_val[4] = 2;
-  coo_val[5] = 1;
-  coo_val[6] = 2;
-  coo_row[0] = 0;
-  coo_row[1] = 1;
-  coo_row[2] = 2;
-  coo_row[3] = 3;
-  coo_row[4] = 4;
-  coo_row[5] = 4;
-  coo_row[6] = 4;
-  coo_col[0] = 3;
-  coo_col[1] = 0;
-  coo_col[2] = 2;
-  coo_col[3] = 1;
-  coo_col[4] = 2;
-  coo_col[5] = 3;
-  coo_col[6] = 4;
+  uint32_t csc_col_gold[6] = {0,1,2,4,6,7};
+  uint32_t csc_row_gold[7] = {1,3,2,4,0,4,4};
 
   // Call coo2csc for isOneBase false
-  coo2csc(coo_row, coo_col, &csc_row[0], &csc_col[0], nnz, rows, cols,
-          isOneBase);
+  coo2csc(csc_row, csc_col,
+          coo_row, coo_col,
+          nnz, n,
+          isOneBased);
+
+  // Verify output
+  uint32_t pass = 1;
+  for (int i = 0; i < n + 1; i++) {
+    printf("%d ", csc_col[i]);
+    pass &= (csc_col[i] == csc_col_gold[i]);
+  }
+  printf("\n");
+  for (uint32_t i = 0; i < nnz; i++) {
+    printf("%d ", csc_row[i]);
+    pass &= (csc_row[i] == csc_row_gold[i]);
+  }
+  printf("\n");
+  if (pass) printf("Tests: PASSed\n");
+  else      printf("Tests: FAILed\n");
+
+  /* cleanup variables */
+  free( csc_row );
+  free( csc_col );
+
+
 }
