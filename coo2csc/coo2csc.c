e7fffe92e437133b17627e5be8cb6660a63b7ee7
Vasilis Naserentin <vasnas@compute.dtcc.chalmers.se>
2020-11-19 06:55:56 +0100

    Changed to simpler example and fixed output.

--- coo2csc/coo2csc.c
+++ coo2csc/coo2csc.c
@@ -47,22 +47,27 @@ void coo2csc(const uint *row_coo, const uint *col_coo, uint *row, uint *col,
   }
   // Debugging
   printf("Columns\n");
-  for (int i = 0; i < nnz; i++) {
+  for (int i = 0; i < cols+1; i++) {
     printf("%i\n", col[i]);
   }
   printf("Rows\n");
-  for (uint i = 0; i < rows + 1; i++) {
+  for (uint i = 0; i < nnz ; i++) {
     printf("%i\n", row[i]);
   }
 }
 
 int main() {
 
-  const uint nnz = 8;  // number of non-zero elements
-  const uint rows = 4; // number of matrix rows
-  const uint cols = 6; // number of matrix columns
+  //const uint nnz = 8;  // number of non-zero elements
+  const uint nnz = 7;
+  //const uint rows = 4; // number of matrix rows
+  const uint rows = 5; // number of matrix rows
 
-  uint *coo_row, *coo_col, *csr_row, *csr_col;
+  //const uint cols = 6; // number of matrix columns
+
+  const uint cols = 5; // number of matrix columns
+
+  uint *coo_row, *coo_col, *csr_row, *csr_col, *csc_col, *csc_row;
   double *coo_val;
 
   coo_row = (uint *)malloc(nnz * sizeof(uint));
@@ -72,6 +77,10 @@ int main() {
   csr_row = (uint *)malloc((rows + 1) * sizeof(uint));
   csr_col = (uint *)malloc(nnz * sizeof(uint));
 
+  csc_row = (uint *)malloc(nnz * sizeof(uint));
+  csc_col = (uint *)malloc(cols+1 * sizeof(uint));
+
+
   // Example from Wikipedia (https://en.wikipedia.org/wiki/Sparse_matrix)
 
   // Matrix:
@@ -80,42 +89,82 @@ int main() {
   //  0  0 50 60 70  0
   //  0  0  0  0  0 80
 
+
+
   // Expected output:
   // csr_val: 10 20 30 40 50 60 70 80
   // csr_col:  0  1  1  3  2  3  4  5
   // csr_row:  0  2  4  7  8
 
-  // create the example simple Matrix in 0-index
+  // create the example simple Matrix in 1-index
+
+
+
+   // 0 0 0 2 0 
+  //  1 0 0 0 0  
+  //  0 0 1 0 0
+  //  0 0 2 1 2
 
   bool isOneBase = false;
 
-  coo_val[0] = 10.0;
-  coo_val[1] = 20.0;
-  coo_val[2] = 30.0;
-  coo_val[3] = 40.0;
-  coo_val[4] = 50.0;
-  coo_val[5] = 60.0;
-  coo_val[6] = 70.0;
-  coo_val[7] = 80.0;
-  coo_row[0] = 0;
+/*
+  coo_val[1] = 10.0;
+  coo_val[2] = 20.0;
+  coo_val[3] = 30.0;
+  coo_val[4] = 40.0;
+  coo_val[5] = 50.0;
+  coo_val[6] = 60.0;
+  coo_val[7] = 70.0;
+  coo_val[8] = 80.0;
   coo_row[1] = 0;
-  coo_row[2] = 1;
+  coo_row[2] = 0;
   coo_row[3] = 1;
-  coo_row[4] = 2;
+  coo_row[4] = 1;
   coo_row[5] = 2;
   coo_row[6] = 2;
-  coo_row[7] = 3;
+  coo_row[7] = 2;
+  coo_row[8] = 3;
 
-  coo_col[0] = 0;
-  coo_col[1] = 1;
+  coo_col[1] = 0;
   coo_col[2] = 1;
-  coo_col[3] = 3;
+  coo_col[3] = 1;
+  coo_col[4] = 3;
+  coo_col[5] = 2;
+  coo_col[6] = 3;
+  coo_col[7] = 4;
+  coo_col[8] = 5;
+*/
+
+//A.data = [2, 1, 1, 2, 2, 1, 2]
+//A.row =  [0, 1, 2, 3, 4, 4, 4]
+//A.col =  [3, 0, 2, 1, 2, 3, 4]
+
+  coo_val[0] = 2;
+  coo_val[1] = 1;
+  coo_val[2] = 1;
+  coo_val[3] = 2;
+  coo_val[4] = 2;
+  coo_val[5] = 1;
+  coo_val[6] = 2;
+  coo_row[0] = 0;
+  coo_row[1] = 1;
+  coo_row[2] = 2;
+  coo_row[3] = 3;
+  coo_row[4] = 4;
+  coo_row[5] = 4;
+  coo_row[6] = 4;
+  coo_col[0] = 3; 
+  coo_col[1] = 0;
+  coo_col[2] = 2;
+  coo_col[3] = 1;
   coo_col[4] = 2;
   coo_col[5] = 3;
   coo_col[6] = 4;
-  coo_col[7] = 5;
+
+
+
 
   // Call coo2csc for isOneBase false
-  coo2csc(coo_row, coo_col, &csr_row[0], &csr_col[0], nnz, rows, cols,
+  coo2csc(coo_row, coo_col, &csc_row[0], &csc_col[0], nnz, rows, cols,
           isOneBase);
 }
